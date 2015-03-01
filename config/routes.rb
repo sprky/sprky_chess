a504cd4aa3a8964adc0bfd5ffe0a119cb0e5f1e0
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-03-01 22:33:03 +0000

    devise is added for use with Player model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sprky::Application.routes.draw do
+  devise_for :players
   root 'static_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
