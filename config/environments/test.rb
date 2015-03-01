423d92ac3f48d6888c4fc5cc6cf5f39829bbc81c
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-03-01 22:46:58 +0000

    email on test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
 Sprky::Application.configure do
-  config.action_mailer.default_url_options = { :host => 'test.com'}
+  config.action_mailer.default_url_options = { :host => 'sprky_test.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
