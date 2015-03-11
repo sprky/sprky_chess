2842f688287eaceb83b6deff4de297cf42416b22
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-03-11 16:50:45 +0000

    this makes the symbols for the pieces viewable on the gameboard

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,3 +1,10 @@
 class King < Piece
-
+	def initialize(args)
+		super
+		if args[:color] == 1
+			write_attribute(:symbol, "white-king.gif")
+		else
+			write_attribute(:symbol, "black-king.gif")
+		end
+	end
 end
--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,3 +1,10 @@
 class Knight < Piece
-
+	def initialize(args)
+		super
+		if args[:color] == 1
+			write_attribute(:symbol, "white-knight.gif")
+		else
+			write_attribute(:symbol, "black-knight.gif")
+		end
+	end
 end
