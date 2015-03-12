cf03bae498be6a26bbccc84e720f84632d3f9353
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-03-12 18:47:06 +0000

    updated test

--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class PieceTest < ActiveSupport::TestCase
   test "piece is marked as captured" do
-    pawn = Piece.where(:x_position => 1, :y_position => 0).last
-    Piece.capture(1, 0)
+    pawn = Piece.where(:x_position => 1, :y_position => 1).last
+    Piece.capture(1, 1)
     assert_equal(pawn.capture?, true)
-    assert_equal(pawn.x_position, 0)
-    assert_equal(pawn.y_position, 0)
+    assert_equal(pawn.x_position, nil)
+    assert_equal(pawn.y_position, nil)
   end
 end
