3305add634b3ea87bea83ba91443e4449226b24f
MCHLWRRN <me@mchlwrrn.com>
2015-03-11 17:05:21 -0700

    Move obstructed logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -35,3 +35,4 @@ class GamesController < ApplicationController
 	end
 
 end
+
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -6,4 +6,15 @@ class Piece < ActiveRecord::Base
   def is_valid_move?
   end
 
+  # Treating every piece as a rook
+  def is_move_obstructed?(destination_x, destination_y)
+  	(2..5).each do |row|
+  		if !self.game.pieces.where(x_position: 0, y_position: 4).first.nil?
+  			puts row
+  			return true
+  		end
+  	end
+  	return false
+  end
+
 end
