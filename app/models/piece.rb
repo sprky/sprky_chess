6df04c4ec9c908a480456fe57d519a3bea667ff9
Aaron Washburn <aaron@it-kutch.com>
2015-03-12 08:46:21 -0400

    moved data around a bit for debugging

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -8,9 +8,11 @@ class Piece < ActiveRecord::Base
 
   # Treating every piece as a rook
   def is_move_obstructed?(destination_x, destination_y)
-  	(2..5).each do |row|
-  		if !self.game.pieces.where(x_position: 0, y_position: 4).first.nil?
-  			puts row
+  	(2..7).each do |row|
+      piece = self.game.pieces.where(x_position: 0, y_position: row).first
+      puts row
+      puts piece
+  		if !piece.nil?
   			return true
   		end
   	end
