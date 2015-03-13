ba1997a3392f55fd8dead113c92d52b4ec3b6389
John Ellison <info@johnwebsites.com>
2015-03-13 14:55:32 -0600

    fix and refactor pawn legal_move?

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,16 +3,13 @@ class Game < ActiveRecord::Base
 
   has_many :players
   has_many :pieces
-  require 'piece'
-  
-
 
   def initialize_board!
   	#white pawns
     (0..7).each do |i|
       Pawn.create(
         player_id: white_player_id,
-        game_id: self.id,
+        game_id: id,
         x_position: i,
         y_position: 1,
         color: 1
@@ -39,7 +36,7 @@ class Game < ActiveRecord::Base
     (0..7).each do |i|
       Pawn.create(
         player_id: black_player_id,
-        game_id: self.id,
+        game_id: id,
         x_position: i,
         y_position: 6,
         color: 0
