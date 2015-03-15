592a0efe3a964ab2a39ba091b370b8f4ac07a1d4
Aaron Washburn <aaron@it-kutch.com>
2015-03-15 10:37:06 -0400

    Modify pawn legal_move? logic

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,15 +1,34 @@
 require 'byebug'
+
 class Pawn < Piece
+  
   def legal_move?(x, y)
     # if move_obstructed?
       # if one space diagonal-left || one space diagonal-right
         # return true
       # end
     # else
+    x_diff = (x_position - x).abs
 
+    # ensure x-value didn't change
+    if x_diff.zero? 
+      puts "backwards_move  #{backwards_move?(y)}"
+      
       return false if backwards_move?(y)
+      
+      puts "proper_length #{proper_length?(y)}"
       proper_length?(y)
-
+    elsif x_diff == 1
+      # did it change by 1 check for capture
+      #  check for capture move
+      puts "check captured"
+      return false
+      # end
+    else  # otherwise has illegal horizontal component
+      puts "horizontal"
+      return false
+    end
+    
     # end (from move_obstructed )
   end
 
@@ -25,7 +44,7 @@ class Pawn < Piece
 
   def proper_length?(y)
     y_diff = (y - y_position).abs
-    first_move?(y) ? y_diff == (1 || 2) : y_diff == 1
+    first_move?(y) ? (y_diff == 1 || y_diff == 2) : y_diff == 1
   end
 
 end
