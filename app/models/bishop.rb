0df48ed8dcb7bc9b1fdeb561089d70eb919e0b87
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-03-16 17:41:43 +0000

    initial attempt at bishop obstruction

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -6,4 +6,53 @@ class Bishop < Piece
 
     x_diff == y_diff ? true : false
   end
+
+  def obstructed_move?(x,y)
+  	pos_x = self.x_position
+  	pos_y = self.y_position
+  	game = self.game
+
+  	#(this takes you up and right)
+  	if pos_x < x && pos_y < y 
+  		puts "meets condition"
+  		while x >= pos_x && y >= pos_y
+  			puts "enters while"
+  			if game.obstruction?(x, y)
+  				return true
+  			end
+  			x += 1
+  			y += 1
+  			puts x, y
+
+  		end
+  		#(this takes you down and right)
+  	#elsif pos_x < x && pos_y > y
+  	#	while x < pos_x && y > pos_y
+  	#		if game.obstruction?(x,y)
+  	#			return true
+  	#		end
+  	#		x += 1
+  	#		y -= 1
+  	#	end
+  		#(this takes you down and left)
+  	#elsif pos_x > x && pos_y > y 
+  	#	while x > pos_x && y > pos_y
+  	#		if game.obstruction?(x, y)
+  	#			return true
+  	#		end
+  	#		x -= 1
+  	#		y -= 1
+  	# end
+  		#(this takes you up and left)
+  	# elsif pos_x > x && pos_y < y
+  	# 	while x > pos_x && y < pos_y
+  	# 		if game.obstruction?(x,y)
+  	# 			return true
+  	# 		end
+  	# 		x -= 1
+  	# 		y += 1
+  	# 	end
+  	end
+  	return false
+  end
 end
--- test/models/bishop_test.rb
+++ test/models/bishop_test.rb
@@ -19,8 +19,26 @@ class BishopTest < ActiveSupport::TestCase
     assert_not @bishop.legal_move?(1, 4)
   end
 
+  test "Bishop is obstructed" do
+    setup_obstruction_tests
+
+    assert_equal true, @bishop.obstructed_move?(5, 6), "quad 1"
+    #assert_equal true, @bishop.obstructed_move?(6, 1), "quad 2"
+    #assert_equal true, @bishop.obstructed_move?(1, 2), "quad 3"
+    #assert_equal true, @bishop.obstructed_move?(1, 6), "quad 4"
+  end
+
   def setup_bishop
     @bishop = FactoryGirl.create(:bishop, x_position: 3, y_position: 4, color: true)
+  end
+
+  def setup_obstruction_tests
+    game = FactoryGirl.create(:game)
+    @bishop = game.pieces.where( type: 'Bishop').last
+    @bishop.x_position = 3
+    @bishop.y_position = 4
+    @pawn = game.pieces.where( x_position: 0, y_position: 0).last
+    @pawn2 = game.pieces.where( x_position: 7, y_position: 7).last
   end
 
 end
