71a47e355262c80bafab9de92c9f3a802c200aa6
John Ellison <info@johnwebsites.com>
2015-03-16 14:21:31 -0600

    add capture move & refactor tests

--- test/models/pawn_test.rb
+++ test/models/pawn_test.rb
@@ -3,77 +3,62 @@ require 'test_helper'
 class PawnTest < ActiveSupport::TestCase
 
   # Illegal White Moves
-  test "illegal white backwards move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 1, color: true)
-    assert_equal false, pawn.legal_move?(1, 0)
+  test "illegal white moves" do
+    game = FactoryGirl.create(:game)
+    white_pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 1, color: true, game_id: game.id)
+    # backwards
+    assert_equal false, white_pawn.legal_move?(1, 0)
+    # horizontal
+    assert_equal false, white_pawn.legal_move?(0, 1)
+    # first move of three
+    assert_equal false, white_pawn.legal_move?(1, 4)
+    # diagonal move
+    assert_equal false, white_pawn.legal_move?(2, 2)
   end
 
-  test "illegal white horizontal move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 1, color: true)
-    assert_equal false, pawn.legal_move?(0, 1)
+  test "legal white first moves" do
+    game = FactoryGirl.create(:game)
+    white_pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 1, color: true, game_id: game.id)
+    # first move of one
+    assert_equal true, white_pawn.legal_move?(1, 2)
+    # first move of two
+    assert_equal true, white_pawn.legal_move?(1, 3)
   end
 
-  test "illegal white first move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 1, color: true)
-    assert_equal false, pawn.legal_move?(1, 4)
+  test "legal white regular moves" do
+    game = FactoryGirl.create(:game)
+    white_pawn = FactoryGirl.create(:pawn, x_position: 2, y_position: 3, color: true, game_id: game.id)
+    assert_equal true, white_pawn.legal_move?(2, 4)
   end
 
-  test "illegal white diagonal move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 1, color: true)
-    assert_equal false, pawn.legal_move?(2, 2)
+  test "illegal black moves" do
+    game = FactoryGirl.create(:game)
+    black_pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 6, color: false, game_id: game.id)
+    # backwards
+    assert_equal false, black_pawn.legal_move?(1, 7)
+    # horizontal
+    assert_equal false, black_pawn.legal_move?(0, 6)
+    # diagonal
+    assert_equal false, black_pawn.legal_move?(2, 5)
+    # first move of three
+    assert_equal false, black_pawn.legal_move?(1, 3)
   end
 
-  # Legal White Moves
-  test "legal white first move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 1, color: true)
-    assert_equal true, pawn.legal_move?(1, 2)
+  test "legal black first moves" do
+    game = FactoryGirl.create(:game)
+    black_pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 6, color: false, game_id: game.id)
+    # first move of one
+    assert_equal true, black_pawn.legal_move?(1, 5)
+    # first move of two
+    assert_equal true, black_pawn.legal_move?(1, 4)
   end
 
-  test "legal white regular move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 2, color: true)
-    assert_equal true, pawn.legal_move?(1, 3)
+  test "legal capture moves" do
+    game = FactoryGirl.create(:game)
+    white_pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 3, color: true, game_id: game.id)
+    black_pawn = FactoryGirl.create(:pawn, x_position: 2, y_position: 4, color: false, game_id: game.id)
+    assert_equal true, white_pawn.legal_move?(2, 4)
+    assert_equal true, black_pawn.legal_move?(1, 3)
   end
-
-  test "legal white capture move" do
-    game = FactoryGirl.create(:game, id: 1)
-    white_pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 3, color: true, game: game.id)
-    black_pawn = FacotyrGirl.create(:pawn, x_position: 2, y_position: 4, color: false, game: game.id)
-
-    assert_equal true, white_pawn.capture_move?(2, 4)
-  end
-
-  # Illegal Black Moves
-  test "illegal backwards black move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 5, color: false)
-    assert_equal false, pawn.legal_move?(1, 6)
-  end
-
-  test "illegal black horizontal move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 6, color: false)
-    assert_equal false, pawn.legal_move?(0, 6)
-  end
-
-  test "illegal black first move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 6, color: false)
-    assert_equal false, pawn.legal_move?(1, 3)
-  end
-
-  test "illegal black diagonal move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 5, color: false)
-    assert_equal false, pawn.legal_move?(0, 4)
-  end
-
-  # Legal Black Moves
-  test "legal black first move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 6, color: false)
-    assert_equal true, pawn.legal_move?(1, 5)
-  end
-
-  test "legal black regular move" do
-    pawn = FactoryGirl.create(:pawn, x_position: 1, y_position: 4, color: false)
-    assert_equal true, pawn.legal_move?(1, 3)
-  end
-
-  private
 
 end
