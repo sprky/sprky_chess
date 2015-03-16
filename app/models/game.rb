57679848b6b1d214c604c500938f9a2b2568c99a
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-03-16 21:43:02 +0000

    adding color

--- app/models/game.rb
+++ app/models/game.rb
@@ -57,11 +57,13 @@ class Game < ActiveRecord::Base
   	King.create(:player_id => black_player_id, :game_id => self.id, :x_position => 4, :y_position => 7, :color => false)
   end
 
-  def capture(x, y)
+  def capture(x, y, piece_color)
     #check if a piece exists at this coordinate
     c=self.pieces.where(x_position:x, y_position:y).last
     #if nil do nothing-- if piece, mark as captured and move to (nil, nil)
     if c == nil
+      return
+    elsif c.color == piece_color
       return
     else
       c.update_attribute(:captured?, true)
