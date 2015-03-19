0ed1e50515a245d98237e65d2f75f553a362f2c1
Aaron Washburn <aaron@it-kutch.com>
2015-03-19 09:35:59 -0400

    Add knight.obstructed_move? and tests

--- test/models/knight_test.rb
+++ test/models/knight_test.rb
@@ -3,20 +3,40 @@ require 'test_helper'
 class PawnTest < ActiveSupport::TestCase
 
   test "valid knight moves" do
-    game = FactoryGirl.create(:game)
-    knight = FactoryGirl.create(:knight, x_position: 4, y_position: 4)
+    setup_knight
 
-    assert knight.valid_move?(5, 6)
-    assert knight.valid_move?(2, 3)
+    assert @knight.valid_move?(5, 6)
+    assert @knight.valid_move?(2, 3)
   end
  
   test "invalid knight moves" do
-    game = FactoryGirl.create(:game)
-    knight = FactoryGirl.create(:knight, x_position: 4, y_position: 4)
-  
-    assert_not knight.valid_move?(5, 5), "diagonal knight move"
-    assert_not knight.valid_move?(4, 6), "vertical knight move"
-    assert_not knight.valid_move?(5, 4), "horizontal knight move"
+    setup_knight
+
+    assert_not @knight.valid_move?(5, 5), "diagonal knight move"
+    assert_not @knight.valid_move?(4, 6), "vertical knight move"
+    assert_not @knight.valid_move?(5, 4), "horizontal knight move"
 
   end
+
+  test "obstructed knight moves" do
+    setup_knight
+
+    assert @knight.obstructed_move?(5, 6)
+    assert @white_knight.obstructed_move?(5, 1)
+  end
+
+  test "unobstructed knight moves" do
+    setup_knight
+
+    assert_not @knight.obstructed_move?(5, 2)
+    assert_not @white_knight.obstructed_move?(5, 5)
+    assert_not @white_knight.obstructed_move?(4, 4), "not obstructed by black knight"
+  end
+
+  def setup_knight
+    @game = FactoryGirl.create(:game)
+    @knight = FactoryGirl.create(:knight, x_position: 4, y_position: 4, color: false, game_id: @game.id)
+    @white_knight = FactoryGirl.create(:knight, x_position: 6, y_position: 3, color: true, game_id: @game.id)
+  end
+
 end
