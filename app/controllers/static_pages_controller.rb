783ecd0bf089152dba0ad2063a9bd13e10e6772f
Aaron Washburn <aaron@it-kutch.com>
2015-03-23 10:58:51 -0400

    Add my games to index page, refactor my games to a partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
-	def index
-	end
+	
+  def index
+    if player_signed_in?
+      @my_games = Game.where("white_player_id = ? or black_player_id = ?", current_player.id, current_player.id).order( 'updated_at').to_a
+    end
+  end
+
 end
