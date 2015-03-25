e9a0812887086b9becc0a0a1f73e565c53658c86
John Ellison <info@johnwebsites.com>
2015-03-25 14:52:05 -0600

    add ajax call for sendMove and setup endpoint for piece#update

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,6 +1,19 @@
 class PiecesController < ApplicationController
   def update
-    
+    @piece = Piece.find(params[:id])
+    @piece.update_attributes(piece_params)
+    game = @piece.game
+
+    render :json => {
+      :update_url => game_path(game)
+    }
+
+  end
+
+  private 
+
+  def piece_params 
+    @piece_params = params.require(:piece).permit(:x_position, :y_position)
   end
 
 end
