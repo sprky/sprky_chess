2d61d05d3705e03fc38bd39a8eca36df092b21a1
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-03-26 09:25:14 -0400

    Added piece controller and piece valid_move tests

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,16 +1,12 @@
 class PiecesController < ApplicationController
+
   def update
     @piece = Piece.find(params[:id])
     x = piece_params[:x_position].to_i
     y = piece_params[:y_position].to_i
 
-    puts x.class
     if @piece.valid_move?(x, y)
-      puts "--------------------------valid_move"
       @piece.update_attributes(piece_params)
-    
-    else
-      puts "---------------------------INVALID MOVE #{x}, #{y}"
     end
 
     
