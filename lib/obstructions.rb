addedd1fcf7ad0cc7998bcd3616cd83c82b047b1
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-03-28 12:21:04 -0400

    Simplify destination_obstructed

--- lib/obstructions.rb
+++ lib/obstructions.rb
@@ -1,10 +1,8 @@
 module Obstructions
   def destination_obstructed?(x, y)
-    destination_obstruction = game.obstruction(x, y) # is there something at the destination?
-    if destination_obstruction && destination_obstruction.color == color
-      # yes and it's the same color - it's an destination_obstruction
-      return true
-    end
+    destination_obstruction = game.obstruction(x, y) # is there something at the destination
+    # if it's the same color as piece it's a destination_obstruction
+    destination_obstruction && destination_obstruction.color == color
   end
 
   def obstructed_diagonally?(x, y)
@@ -30,7 +28,6 @@ module Obstructions
       return true if game.obstruction(pos_x, pos_y)
       pos_x += horizontal_increment
       pos_y += vertical_increment
-
     end
 
     # default to false
@@ -41,6 +38,7 @@ module Obstructions
     # store piece x & y positions in local variables
     pos_x = x_position
     pos_y = y_position
+
     if y == pos_y # move is in x direction
       # determine horizontal increment value
       horizontal_increment = x > pos_x ? 1 : -1
@@ -53,9 +51,7 @@ module Obstructions
         # return true if we find an obstruction
         return true if game.obstruction(pos_x, pos_y)
         pos_x += horizontal_increment
-
       end
-
     elsif x == pos_x # move is in y direction
       # determine vertical increment value
       vertical_increment = y > pos_y ? 1 : -1
@@ -68,7 +64,6 @@ module Obstructions
         # return true if we find an obstruction
         return true if game.obstruction(pos_x, pos_y)
         pos_y += vertical_increment
-
       end
     end
 
