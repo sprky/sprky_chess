5593f22ece2f61b91cf25a7dbe881e13df0f0163
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-03-30 14:17:28 +0000

    cleaned up migrations

--- db/migrate/20150327151750_add_has_movedto_piece.rb
+++
@@ -1,5 +0,0 @@
-class AddHasMovedtoPiece < ActiveRecord::Migration
-  def change
-  	add_column :pieces, :has_moved, :string
-  end
-end
--- db/migrate/20150327162637_switch_has_moved_to_status_in_pieces.rb
+++
@@ -1,6 +0,0 @@
-class SwitchHasMovedToStatusInPieces < ActiveRecord::Migration
-  def change
-  	remove_column :pieces, :has_moved, :string
-  	add_column :pieces, :status, :string
-  end
-end
