eeb3ff31ccf01aa33df31a0b705b968acf227fea
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-04-01 13:18:48 -0400

    Assing player_id to pieces after game join

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,7 +8,6 @@ class Game < ActiveRecord::Base
     # white pawns
     (0..7).each do |i|
       Pawn.create(
-        player_id: white_player_id,
         game_id: id,
         x_position: i,
         y_position: 1,
@@ -17,25 +16,24 @@ class Game < ActiveRecord::Base
     end
 
     # white rooks
-    Rook.create(player_id: white_player_id, game_id: id, x_position: 0, y_position: 0, color: true)
-    Rook.create(player_id: white_player_id, game_id: id, x_position: 7, y_position: 0, color: true)
+    Rook.create(game_id: id, x_position: 0, y_position: 0, color: true)
+    Rook.create(game_id: id, x_position: 7, y_position: 0, color: true)
 
     # white knights
-    Knight.create(player_id: white_player_id, game_id: id, x_position: 1, y_position: 0, color: true)
-    Knight.create(player_id: white_player_id, game_id: id, x_position: 6, y_position: 0, color: true)
+    Knight.create(game_id: id, x_position: 1, y_position: 0, color: true)
+    Knight.create(game_id: id, x_position: 6, y_position: 0, color: true)
 
     # white bishops
-    Bishop.create(player_id: white_player_id, game_id: id, x_position: 2, y_position: 0, color: true)
-    Bishop.create(player_id: white_player_id, game_id: id, x_position: 5, y_position: 0, color: true)
+    Bishop.create(game_id: id, x_position: 2, y_position: 0, color: true)
+    Bishop.create(game_id: id, x_position: 5, y_position: 0, color: true)
 
     # white queen and king
-    Queen.create(player_id: white_player_id, game_id: id, x_position: 3, y_position: 0, color: true)
-    King.create(player_id: white_player_id, game_id: id, x_position: 4, y_position: 0, color: true)
+    Queen.create(game_id: id, x_position: 3, y_position: 0, color: true)
+    King.create(game_id: id, x_position: 4, y_position: 0, color: true)
 
     # black pawns
     (0..7).each do |i|
       Pawn.create(
-        player_id: black_player_id,
         game_id: id,
         x_position: i,
         y_position: 6,
@@ -44,21 +42,26 @@ class Game < ActiveRecord::Base
     end
 
     # black rooks
-    Rook.create(player_id: black_player_id, game_id: id, x_position: 0, y_position: 7, color: false)
-    Rook.create(player_id: black_player_id, game_id: id, x_position: 7, y_position: 7, color: false)
+    Rook.create(game_id: id, x_position: 0, y_position: 7, color: false)
+    Rook.create(game_id: id, x_position: 7, y_position: 7, color: false)
     # black knights
-    Knight.create(player_id: black_player_id, game_id: id, x_position: 1, y_position: 7, color: false)
-    Knight.create(player_id: black_player_id, game_id: id, x_position: 6, y_position: 7, color: false)
+    Knight.create(game_id: id, x_position: 1, y_position: 7, color: false)
+    Knight.create(game_id: id, x_position: 6, y_position: 7, color: false)
     # black bishops
-    Bishop.create(player_id: black_player_id, game_id: id, x_position: 2, y_position: 7, color: false)
-    Bishop.create(player_id: black_player_id, game_id: id, x_position: 5, y_position: 7, color: false)
+    Bishop.create(game_id: id, x_position: 2, y_position: 7, color: false)
+    Bishop.create(game_id: id, x_position: 5, y_position: 7, color: false)
     # black queen and king
-    Queen.create(player_id: black_player_id, game_id: id, x_position: 3, y_position: 7, color: false)
-    King.create(player_id: black_player_id, game_id: id, x_position: 4, y_position: 7, color: false)
+    Queen.create(game_id: id, x_position: 3, y_position: 7, color: false)
+    King.create(game_id: id, x_position: 4, y_position: 7, color: false)
   end
 
   # determind if obstruction occurs at x, y in game
   def obstruction(x, y)
     pieces.where(x_position: x, y_position: y).last
+  end
+
+  def assign_pieces
+    pieces.where(color: true).each { |p| p.update_attributes(player_id: white_player_id) }
+    pieces.where(color: false).each { |p| p.update_attributes(player_id: black_player_id) }
   end
 end
