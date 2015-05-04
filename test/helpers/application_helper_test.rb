1393f182019bfad731cdee982ae0eb4fb382d000
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-05-04 16:25:15 -0400

    Refactor gameboard into helper methods

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -14,17 +14,4 @@ class ApplicationHelperTest < ActionView::TestCase
 
     assert_nil piece_id(piece)
   end
-
-  test 'Should return correct gameboard td for empty square' do
-    expected = "<td class='x-position-1'' data-x-position='1' data-y-position='6' data-piece-id='' data-piece-type=''></td>"
-
-    assert_equal expected, gameboard_td(nil, 1, 6)
-  end
-
-  test 'Should return correct gameboard td for occupied square' do
-    knight = FactoryGirl.create(:knight, color: false)
-    expected = "<td class='x-position-6'' data-x-position='6' data-y-position='7' data-piece-id='#{knight.id}' data-piece-type='#{knight.type}'><img alt=\"Black knight\" src=\"/images/black-knight.svg\" /></td>"
-
-    assert_equal expected, gameboard_td(knight, 6, 7)
-  end
 end
