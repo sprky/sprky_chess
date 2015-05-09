cff98f69cce0f351b359b25ba130e166e3e1a207
John Ellison <info@johnwebsites.com>
2015-05-08 18:17:12 -0600

    refactored javascript

--- app/assets/javascripts/gameboard.js
+++ app/assets/javascripts/gameboard.js
@@ -1,33 +1,89 @@
 $(document).ready(function () {
-  // Moves
 
-  $('#gameboard td').click( function() {
+  // Selecting & Moving Pieces
+  $( '#gameboard td' ).click( function() {
     $this = $(this);
-    var pieceIsSelected = $this.hasClass('piece-selected');
-    var squareHasPiece = $this.find('img').length;
+    var pieceIsSelected = $( '#gameboard td' ).hasClass( 'selected' );
+    var squareHasPiece = $this.find( 'img' ).length;
+    var $pieceSelected = pieceSelected();
 
-    if (squareHasPiece) {
-      selectPiece($this);
-    } 
-
-    if (pieceIsSelected) {
-      deselectPiece($this);
+    if ( pieceIsSelected ) {
+      if ( $pieceSelected.data( 'piece-id' ) == $this.data( 'piece-id' ) ) {
+        deselectPiece( $this );
+      } else {
+          sendMove ( $pieceSelected, $this );
+        }
+    } else {
+      selectPiece( $this );
     }
+
   });
 
-  function togglePieceSelect(piece) {
+  function pieceSelected() {
+    return $( '#gameboard td.selected' );
+  }
+
+  function sendMove ( $pieceSelected, $destination ) {
+    console.log ( 'sent ' + $pieceSelected.data( 'piece-id' ) + ' to '
+     + $destination.data( 'x-position' ) + ' ' 
+     + $destination.data( 'y-position' ) );
+
+    var piece = {
+      id: $pieceSelected.data( 'piece-id' ),
+      x_position: $destination.data( 'x-position' ),
+      y_position: $destination.data( 'y-position' )
+    }
+
+    if ( isPawn( $pieceSelected ) && isMovingToLastRank( piece.y_position )) {
+      openModal('#promo-modal', function( pieceType ) {
+        piece.type = pieceType;
+        submitAjax( piece );
+      });
+
+    } else {
+      submitAjax( piece );
+    }
 
   }
 
-  function selectPiece(piece) {
-    console.log('selected a ' + piece.data('piece-type'));
-    piece.addClass('piece-selected');
-    return piece;
+  function submitAjax( piece ) {
+
+    var pieceId = piece.id;
+
+    // removes the id property from piece
+    delete piece.id;
+
+    $.ajax({
+      type: 'PATCH',
+      url: '/pieces/' + pieceId,
+      dataType: 'json',
+      data: { 
+        piece: piece
+      },
+      success: function(data) {
+        $(location).attr('href', data.update_url);
+      }
+    });
   }
 
-  function deselectPiece(piece) {
-    console.log('deslected a ' + piece.data('piece-type'));
-    piece.removeClass('piece-selected');
+  function selectedSamePiece( $piece ) {
+    console.log( 'selected same piece' );
+    var pieceId = $piece.data( 'piece-id' );
+    // need to grab pieceId from first pieceIsSelected in click handler
+  }
+
+  function selectPiece( $piece ) {
+    var isPlayersTurn = $( '#gameboard' ).data( 'your-turn' );
+
+    if ( isPlayersTurn ) {
+      console.log( 'selected a ' + $piece.data( 'piece-type' ) );
+      $piece.addClass( 'selected' );
+    }
+  }
+
+  function deselectPiece( $piece ) {
+    console.log( 'deslected a ' + $piece.data( 'piece-type' ) );
+    $piece.removeClass( 'selected' );
   }
 
   function selectDestination() {
@@ -35,12 +91,12 @@ $(document).ready(function () {
   }
 
   // Pawn Promotion
-  function isPawn(piece) {
-
+  function isPawn( $piece ) {
+    return ( $piece.data('piece-type') == 'Pawn')
   }
 
-  function isMovingToLastRank(piece, y) {
-
+  function isMovingToLastRank( y ) {
+    return ( ( y == 0 ) || ( y == 7 ) ) 
   }
 
   function isPawnPromotion(piece, y) {
@@ -49,6 +105,31 @@ $(document).ready(function () {
     } else {
       return false;
     }
+  }
+
+  // Modal
+  function openModal ( modalId, callback ) {
+    var $modal = $(modalId);
+
+    $modal.prop("checked", true);
+
+    if ($modal.is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+
+    $(".modal-fade-screen, .modal-close").on("click", function() {
+      $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal-inner").on("click", function(e) {
+      e.stopPropagation();
+    });
+
+    $('.promo-selection-submit input').on('click', function() {
+      callback( $('.promo-selection-choice input').val() );
+    });
   }
 
   // Logic Methods
