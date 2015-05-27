3ab51abf741be5425412986e1da6d59514bd6edb
John Ellison <info@johnwebsites.com>
2015-05-26 20:34:41 -0600

    update js code sample and description.

--- app/assets/javascripts/gameboard.js
+++ app/assets/javascripts/gameboard.js
@@ -1,49 +1,49 @@
 $(document).ready(function () {
 
   // Selecting & Moving Pieces
-  $( '#gameboard td' ).click( function() {
+  $('#gameboard td').click(function() {
     $this = $(this);
     // Returns boolean to determine piece is selected or not
-    var pieceIsSelected = $( '#gameboard td' ).hasClass( 'selected' );
+    var pieceIsSelected = $('#gameboard td').hasClass('selected');
 
-    if ( pieceIsSelected ) {
+    if (pieceIsSelected){
       // Returns jQuery object of selectedPiece if a piece is selected
       var $piece = selectedPiece();
 
       if ($piece.data('piece-id') == $this.data('piece-id')) {
         deselectPiece( $this );
       } else {
-          sendMove ( $piece, $this );
+          sendMove ($piece, $this);
         }
     } else {
-      selectPiece( $this );
+      selectPiece($this);
     }
 
   });
 
   function selectedPiece() {
-    return $( '#gameboard td.selected' );
+    return $('#gameboard td.selected');
   }
 
-  function sendMove ( $piece, $destination ) {
+  function sendMove ($piece, $destination) {
     var piece = {
-      id: $piece.data( 'piece-id' ),
-      x_position: $destination.data( 'x-position' ),
-      y_position: $destination.data( 'y-position' )
+      id: $piece.data('piece-id'),
+      x_position: $destination.data('x-position'),
+      y_position: $destination.data('y-position')
     }
 
-    if ( isPawnPromotion( $piece, piece.y_position ) ) {
-      openModal('#promo-modal', function( pieceType ) {
+    if ( isPawnPromotion($piece, piece.y_position)) {
+      openModal('#promo-modal', function(pieceType) {
         piece.type = pieceType;
-        submitPieceUpdate( piece );
+        submitPieceUpdate(piece);
       });
 
     } else {
-      submitPieceUpdate( piece );
+      submitPieceUpdate(piece);
     }
   }
 
-  function submitPieceUpdate( piece ) {
+  function submitPieceUpdate(piece) {
     $.ajax({
       type: 'PATCH',
       url: '/pieces/' + piece.id,
@@ -57,33 +57,33 @@ $(document).ready(function () {
     });
   }
 
-  function selectPiece( $piece ) {
-    var isPlayersTurn = $( '#gameboard' ).data( 'your-turn' );
+  function selectPiece($piece) {
+    var isPlayersTurn = $('#gameboard').data('your-turn');
 
-    if ( isPlayersTurn ) {
-      $piece.addClass( 'selected' );
+    if (isPlayersTurn) {
+      $piece.addClass('selected');
     }
   }
 
-  function deselectPiece( $piece ) {
-    $piece.removeClass( 'selected' );
+  function deselectPiece($piece) {
+    $piece.removeClass('selected');
   }
 
   // Pawn Promotion
-  function isPawn( $piece ) {
-    return ( $piece.data('piece-type') == 'Pawn');
+  function isPawn($piece) {
+    return ($piece.data('piece-type') == 'Pawn');
   }
 
-  function isMovingToLastRank( y ) {
-    return ( ( y == 0 ) || ( y == 7 ) );
+  function isMovingToLastRank(y) {
+    return ((y == 0) || (y == 7));
   }
 
   function isPawnPromotion( $piece, y) {
-    return ( isPawn( $piece ) && isMovingToLastRank( y ) ); 
+    return (isPawn($piece) && isMovingToLastRank(y)); 
   }
 
   // Modal
-  function openModal ( modalId, callback ) {
+  function openModal (modalId, callback) {
     var $modal = $(modalId);
 
     // Change modal-state checkbox to checked
